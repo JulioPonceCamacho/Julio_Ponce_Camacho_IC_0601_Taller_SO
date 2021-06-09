4c599089714f3e1578ef007f8a2af3e02afa2f9a
Julio Ponce <2018150480788@tesjo.edu.mx>
2021-06-08 20:31:20 -0500

    Actualizacion 2. Programa para el manejo de archivos

--- Competencia_3,4_Practica_1/control_archivo.c
+++ Competencia_3,4_Practica_1/control_archivo.c
@@ -14,7 +14,7 @@ int existe_archivo(char *nombre_arch){
 
 void cerrar_archivo(FILE *ptrArchivo, char *nombre_arch){
 	fclose(ptrArchivo);
-	printf("\n----Archivo [%s]  Cerrado Correctamente----", nombre_arch);
+	printf("\n--->> Archivo [%s]  Cerrado Correctamente <<----", nombre_arch);
 }
 
 FILE *abrir_Archivo_lectura_escritura(char *nombre_arch){
@@ -22,10 +22,10 @@ FILE *abrir_Archivo_lectura_escritura(char *nombre_arch){
 	ptrArchivo = fopen(nombre_arch, "r+");
 	if( ptrArchivo == NULL ){
 		// con r+ Abre un archivo para actualización (lectura y escritura).
-		printf("---> El archivo -> %s <- NO pudo abrirse (uso de r+).\n", nombre_arch);
+		printf("\n->> El archivo [ %s ] NO pudo abrirse (uso de r+). <<-", nombre_arch);
 	}	
 	else{
-		printf("---> Archivo -> %s <- Abierto (uso de r+).\n", nombre_arch);
+		printf("\n->> Archivo [ %s ] Abierto (uso de r+). <<-", nombre_arch);
 	}
 	return ptrArchivo;
 }
@@ -35,11 +35,11 @@ int crear_archivo(char *nombre_arch){
 	ptrArchivo = fopen(nombre_arch, "w");
 	if( ptrArchivo == NULL ){
 		// con w crea un archivo para escritura. Si el archivo ya existe, descarta el contenido actual.
-		printf("----> El archivo -> %s <- no pudo crearse (abrir; uso de w).\n", nombre_arch);
+		printf("\n->> El archivo [ %s ] no pudo crearse (abrir; uso de w). <<-", nombre_arch);
 	}	
 	else{
 		creado = 1;
-		printf("----> Archivo -> %s <- Creado (abierto; uso de w).\n", nombre_arch);
+		printf("\n-->> Archivo [ %s ] Creado (abierto; uso de w). <<--", nombre_arch);
 		cerrar_archivo(ptrArchivo, nombre_arch);
 	}
 	return creado;
--- Competencia_3,4_Practica_1/main.c
+++ Competencia_3,4_Practica_1/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]){
     printf("\n\t::::::::::::::::::::::::::::::::::::::::::");
     printf("\n\t::::::::::: MANEJO DE FICHEROS :::::::::::");
     printf("\n\t::::::::::::::::::::::::::::::::::::::::::\n");
+    printf("\n-->Numero de argumentos=  %d",argc);
     printf("\n-->Nombre ejecutable  [Argumento 1]=  %s",argv[0]);
     printf("\n-->Archivo de entrada [Argumento 2]=  %s",argv[1]);
     printf("\n-->Archivo de Salida  [Argumento 3]=  %s",argv[2]);    
@@ -12,9 +13,18 @@ int main(int argc, char *argv[]){
     char *Salida=argv[2];
     struct lista *LISTA=NULL;
     char palabras[TAM_NOMBRE];
-    int i=0;    
+    int i=0;
     FILE *archivo_Entrada=NULL;
     FILE *archivo_Salida=NULL;
+    
+    if(argc==1) {
+        printf("\n\n Porfavor, indique un archivo de entrada y el nombre de uno de salida como argumentos\n\t Saliendo... \n\n");
+        return 0;
+   }
+   else if(argc==2) {
+        printf("\n\n Porfavor, indique el nombre de un archivo de salida como segundo argumento (No es necesario que este exista) \n\t Saliendo...\n\n");
+        return 0;
+    }
 
     printf("\n\nComprobando si existe el archivo de entrada %s",Entrada);
     if (existe_archivo(Entrada)){
@@ -27,7 +37,7 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    printf("\nDatos en el archivo [%s] que se almacenan en una lista linkeada",argv[1]);
+    printf("\n\nDatos en el archivo [%s] que se almacenan en una lista linkeada",argv[1]);
     printf("\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
     while(!feof(archivo_Entrada)){
         fscanf(archivo_Entrada,"%s",palabras);
@@ -39,22 +49,24 @@ int main(int argc, char *argv[]){
     if(i>0){
     ordenar_lista(LISTA);
 ////Abriendo o creando el archivo de salida
+
     printf("\n\nComprobando si existe el archivo de salida %s",Salida);
     if (existe_archivo(Salida)){
-        printf("\n--->> Existe el archivo de Salida <----");
+        printf("\n--->> Existe el archivo de Salida <<---\n\t");
         archivo_Entrada=abrir_Archivo_lectura_escritura(Salida);
     }
     else{
-        printf("\n--->> NO Existe el archivo de entrada <----");
-        printf("\n Creando archivo [%s]",Salida);
+        printf("\n--->> NO Existe el archivo de entrada <<---");
+        printf("\n Creando archivo [%s]\n\t",Salida);
         crear_archivo(Salida);
     }
     archivo_Salida=abrir_Archivo_lectura_escritura(Salida);
     for(int j=1;j<=i;j++){
         fprintf(archivo_Salida,"%s\n",obtener_elemento(LISTA,j));
     }
-    printf("\nNOTA: Los datos ordenados se han escrito en el archivo [%s]\n",Salida);
-
+    printf("\n\t____________________________________________________________________");
+    printf("\n\t   NOTA: Los datos ordenados se han escrito en el archivo [%s] ",Salida);
+    printf("\n\t--------------------------------------------------------------------\n");
     cerrar_archivo(archivo_Entrada,Entrada);
     cerrar_archivo(archivo_Salida,Salida);
     free(LISTA);
